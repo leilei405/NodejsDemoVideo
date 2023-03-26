113484c6b2c05f16f5b959b2d62fc785331645fe
fll <fllning@163.com>
2023-03-26 16:59:02 +0800

    feat: 使用commander命令参数处理工具

--- bin/vite.js
+++ bin/vite.js
@@ -1,7 +1,12 @@
 #! /usr/bin/env node
+const { program } = require('commander');
 
-if (process.argv[2] === '--help') {
-    console.log('我获取到了命令行参数');
-}
-console.log('my-vite');
-console.log(process.argv);
+// if (process.argv[2] === '--help') {
+//     console.log('我获取到了命令行参数');
+// }
+// console.log('my-vite');
+// console.log(process.argv);
+program
+  .option('-f --first <first>', 'First'); // <> 意思就是如果使用了-f 那么后面必须要跟对应的参数
+  .option('-s, --separator <char>');
+program.parse(process.argv);  // 命令行所有的参数选项
